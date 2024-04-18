10957906910726d195664bb8fbb3505ec90119c5
DavBN <162839191+DavBN@users.noreply.github.com>
2024-04-18 11:34:22 -0500

    Login casi listo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gordo/app-salon',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e12a4482b91350f0b811c75bdc25a41fa9784bf9',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gordo/app-salon' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e12a4482b91350f0b811c75bdc25a41fa9784bf9',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
