aa87b6b91e82b8f3ff601f2ed4abf68f4760c8bd
DavBN <162839191+DavBN@users.noreply.github.com>
2024-04-23 10:44:37 -0500

    Login terminado, botones terminados con funciones javascript funcionando, datos de citas funcionando

---
+++ views/cita/index.php
@@ -0,0 +1,64 @@
+<h1 class="nombre-pagina">Crear Nueva Cita</h1>
+<p class="descripcion-pagina">Elige tus servicios con tus datos</p>
+
+<div id="app">
+    <nav class="tabs">
+        <button class="actual" type="button" data-paso="1">Servicios</button>
+        <button type="button" data-paso="2">Información cita</button>
+        <button type="button" data-paso="3">Resumen</button>
+    </nav>
+
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p class="text-center">Elige tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus datos y cita</h2>
+        <p class="text-center">Coloca tus datos y fecha de cita</p>
+
+        <form class="formulario">
+            <div class="campo">
+                <label for="nombre">Nombre</label>
+                <input id="nombre" type="text" placeholder="Tu nombre" value="<?php echo $nombre; ?>" disabled />
+            </div>
+
+            <div class="campo">
+                <label for="nombre">Fecha</label>
+                <input id="fecha" type="date" />
+            </div>
+
+            <div class="campo">
+                <label for="nombre">Hora</label>
+                <input id="hora" type="time" />
+            </div>
+        </form>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p class="text-center">Verifica que la información sea corrrecta</p>
+
+    </div>
+
+    <div class="paginacion">
+        <button 
+        id="anterior"
+        class="boton"
+        >&laquo; Anterior </button>
+
+        <button 
+        id="siguiente"
+        class="boton"
+        >Siguiente &raquo; </button>
+    </div>
+
+</div>
+
+<?php 
+   $script = "
+   
+   <script src='build/js/app.js'></script>
+   ";
+
+
+?>
